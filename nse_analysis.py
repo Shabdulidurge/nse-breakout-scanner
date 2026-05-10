06f03d2161b29994c7ead9aa119039570dcc80a3
Shabdulidurge <43006070+Shabdulidurge@users.noreply.github.com>
2026-05-10 21:44:31 +0530

    Fetch and update stock data from NSE with zip file

--- nse_analysis.py
+++ nse_analysis.py
@@ -4,6 +4,8 @@ import requests
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import json
+from datetime import datetime
+from zipfile import ZipFile
 
 # Read credentials from GitHub Secrets
 google_credentials_json = os.environ['GOOGLE_CREDENTIALS']
@@ -26,12 +28,41 @@ sheet = client.open("Stock Analysis NSE Python")
 
 worksheet = sheet.worksheet("Final List")
 
-data = [
-    ["Stock", "Price", "Volume"],
-    ["RELIANCE", 2500, 100000]
-]
+today = datetime.now().strftime("%d%m%Y")
+
+url = f"https://nsearchives.nseindia.com/content/cm/BhavCopy_NSE_CM_0_0_0_{today}_F_0000.csv.zip"
+
+zip_file = "bhavcopy.zip"
+
+response = requests.get(url)
+
+with open(zip_file, "wb") as file:
+    file.write(response.content)
+
+with ZipFile(zip_file, 'r') as zip_ref:
+    zip_ref.extractall()
+
+csv_file = [f for f in os.listdir() if f.endswith(".csv")][0]
+
+df = pd.read_csv(csv_file)
+
+df = df[[
+    'SYMBOL',
+    'OPEN_PRICE',
+    'HIGH_PRICE',
+    'LOW_PRICE',
+    'CLOSE_PRICE',
+    'TOTTRDQTY'
+]]
+
+top_stocks = df.sort_values(
+    by='TOTTRDQTY',
+    ascending=False
+).head(50)
+
+data = [top_stocks.columns.tolist()] + top_stocks.values.tolist()
 
 worksheet.clear()
 worksheet.update("A1", data)
 
-print("Updated Successfully")
+print("Bhav Copy Updated Successfully")

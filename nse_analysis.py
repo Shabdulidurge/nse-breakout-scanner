90beafadc3dbf24afc6972b912e97f00362635b6
Shabdulidurge <43006070+Shabdulidurge@users.noreply.github.com>
2026-05-10 22:19:35 +0530

    Refactor NSE data fetching and update Google Sheets

--- nse_analysis.py
+++ nse_analysis.py
@@ -1,15 +1,15 @@
 import os
-import pandas as pd
-import requests
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
 import json
-from datetime import datetime
-from zipfile import ZipFile
+import pandas as pd
+import gspread
+
+from oauth2client.service_account import ServiceAccountCredentials
+from nsepython import *
 
 # Read credentials from GitHub Secrets
 google_credentials_json = os.environ['GOOGLE_CREDENTIALS']
 
+# Google Sheets Authentication
 scope = [
     "https://spreadsheets.google.com/feeds",
     "https://www.googleapis.com/auth/drive"
@@ -24,67 +24,49 @@ creds = ServiceAccountCredentials.from_json_keyfile_dict(
 
 client = gspread.authorize(creds)
 
+# Open Google Sheet
 sheet = client.open("Stock Analysis NSE Python")
 
 worksheet = sheet.worksheet("Final List")
 
-#today = "08MAY2026"
+# Fetch NSE Data
+stocks = nsefetch(
+    "https://www.nseindia.com/api/equity-stockIndices?index=NIFTY%20250"
+)
 
-url = f"https://archives.nseindia.com/content/historical/EQUITIES/2025/MAY/cm08MAY2025bhav.csv.zip"
+df = pd.DataFrame(stocks['data'])
 
-print(url)
-
-zip_file = "bhavcopy.zip"
-
-session = requests.Session()
-
-headers = {
-    "User-Agent": "Mozilla/5.0",
-    "Accept-Language": "en-US,en;q=0.9",
-    "Accept-Encoding": "gzip, deflate, br",
-    "Referer": "https://www.nseindia.com/"
-}
-
-# First visit NSE homepage
-session.get("https://archives.nseindia.com", headers=headers)
-
-# Download bhavcopy
-response = session.get(url, headers=headers)
-
-# Check if download successful
-if response.status_code == 200 and 'application/zip' in response.headers.get('Content-Type', ''):
-
-    with open(zip_file, "wb") as file:
-        file.write(response.content)
-    with ZipFile(zip_file, 'r') as zip_ref:
-        zip_ref.extractall()
-
-else:
-    raise Exception(
-        f"Failed to download ZIP. Status: {response.status_code}, Content-Type: {response.headers.get('Content-Type')}"
-    )
-
-csv_file = [f for f in os.listdir() if f.endswith(".csv")][0]
-
-df = pd.read_csv(csv_file)
-
+# Select Required Columns
 df = df[[
+    'symbol',
+    'open',
+    'dayHigh',
+    'dayLow',
+    'lastPrice',
+    'totalTradedVolume'
+]]
+
+# Rename Columns
+df.columns = [
     'SYMBOL',
     'OPEN_PRICE',
     'HIGH_PRICE',
     'LOW_PRICE',
     'CLOSE_PRICE',
     'TOTTRDQTY'
-]]
+]
 
+# Sort by Volume
 top_stocks = df.sort_values(
     by='TOTTRDQTY',
     ascending=False
 ).head(50)
 
+# Convert Data for Google Sheets
 data = [top_stocks.columns.tolist()] + top_stocks.values.tolist()
 
+# Update Google Sheet
 worksheet.clear()
 worksheet.update("A1", data)
 
-print("Bhav Copy Updated Successfully")
+print("NSE Data Updated Successfully")
